9bcf51e624a4153b57febc35b7eb28db3ac1cbcf
Guillermo Alvarez <guillermo@vaadin.com>
2015-02-11 14:55:40 +0200

    Vaadin Spreadsheet Tutorial initial commit
    Change-Id: I1fe3c1bb2a6b03a47a82a6b1ade4df7c69202869

---
+++ README.md
@@ -0,0 +1,26 @@
+Vaadin Spreadsheet Tutorial
+==============
+
+This is the final code produced by the [Vaadin Spreadsheet Tutorial](https://vaadin.com/wiki/-/wiki/-/Vaadin+Spreadsheet+Tutorial).
+
+
+Running the example
+-------------------
+The following command starts a jetty server, the result is then browseable by navigating to http://localhost:8080
+<pre>mvn jetty:run</pre>
+
+
+Importing in Eclipse
+--------------------
+Make sure you have "Eclipse IDE for Java EE Developers" and Maven integration "m2e-wtp" installed. You will get Eclipse from http://eclipse.org/downloads/ and plugins through Help -> Eclipse Marketplace... menu
+
+To checkout and run the project from Eclipse, do:
+- File -> Import...
+- Check out Maven Projects from CMS
+- Choose Git from SCM menu and set URL to git://github.com/vaadin/spreadsheet-tutorial
+  - If you do not see "Git" in the SCM menu, click "Find more SCM connectors in the m2e Marketplace" and install "m2e-egit"
+- Now you should have a "spreadsheet-tutorial" project in your workspace
+- To run it, right click and choose Run As -> Run on Server
+- Start experimenting
+
+Note that if you are missing EGit plugin, "Maven SCM Handler for EGit" or a local server to run the project on, you will be asked to install these while doing the above.
---
+++ src/main/java/com/vaadin/MyUI.java
@@ -0,0 +1,205 @@
+package com.vaadin;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.annotation.WebServlet;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.Font;
+import org.apache.poi.ss.util.CellReference;
+import org.apache.poi.xssf.usermodel.XSSFCellStyle;
+import org.apache.poi.xssf.usermodel.XSSFColor;
+import org.apache.poi.xssf.usermodel.XSSFFont;
+
+import com.vaadin.addon.spreadsheet.Spreadsheet;
+import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.annotations.Widgetset;
+import com.vaadin.server.FontAwesome;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinServlet;
+import com.vaadin.shared.ui.colorpicker.Color;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Button.ClickListener;
+import com.vaadin.ui.ColorPicker;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.components.colorpicker.ColorChangeEvent;
+import com.vaadin.ui.components.colorpicker.ColorChangeListener;
+
+/**
+ * Final code produced by the Vaadin Spreadsheet Tutorial <br>
+ * Available in https://vaadin.com/wiki/-/wiki/-/Vaadin+Spreadsheet+Tutorial <br>
+ * Read the tutorial for explanations and details of the code below
+ */
+@Theme("mytheme")
+@Widgetset("com.vaadin.MyAppWidgetset")
+public class MyUI extends UI {
+
+    private Spreadsheet spreadsheet = null;
+
+    @Override
+    protected void init(VaadinRequest vaadinRequest) {
+        final VerticalLayout layout = new VerticalLayout();
+        layout.setSizeFull();
+        setContent(layout);
+
+        HorizontalLayout styleToolbar = createStyleToolbar();
+        layout.addComponent(styleToolbar);
+        layout.setExpandRatio(styleToolbar, 0);
+        initSpreadsheet();
+        layout.addComponent(spreadsheet);
+        layout.setExpandRatio(spreadsheet, 1);
+    }
+
+    private void initSpreadsheet() {
+        File sampleFile = new File("sample.xlsx");
+        try {
+            spreadsheet = new Spreadsheet(sampleFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private HorizontalLayout createStyleToolbar() {
+        HorizontalLayout toolbar = new HorizontalLayout();
+        Button boldButton = new Button(FontAwesome.BOLD);
+        boldButton.addClickListener(new ClickListener() {
+            @Override
+            public void buttonClick(ClickEvent event) {
+                updateSelectedCellsBold();
+            }
+        });
+        ColorPicker backgroundColor = new ColorPicker();
+        backgroundColor.setCaption("Background Color");
+        backgroundColor.addColorChangeListener(new ColorChangeListener() {
+            @Override
+            public void colorChanged(ColorChangeEvent event) {
+                updateSelectedCellsBackgroundColor(event.getColor());
+            }
+        });
+        ColorPicker fontColor = new ColorPicker();
+        fontColor.setCaption("Font Color");
+        fontColor.addColorChangeListener(new ColorChangeListener() {
+            @Override
+            public void colorChanged(ColorChangeEvent event) {
+                updateSelectedCellsFontColor(event.getColor());
+            }
+        });
+        toolbar.addComponent(boldButton);
+        toolbar.addComponent(backgroundColor);
+        toolbar.addComponent(fontColor);
+        return toolbar;
+    }
+
+    private void updateSelectedCellsBold() {
+        if (spreadsheet != null) {
+            List<Cell> cellsToRefresh = new ArrayList<Cell>();
+            for (CellReference cellRef : spreadsheet
+                    .getSelectedCellReferences()) {
+                // Obtain Cell using CellReference
+                Cell cell = getOrCreateCell(cellRef);
+                // Clone Cell CellStyle
+                CellStyle style = cloneStyle(cell);
+                // Clone CellStyle Font
+                Font font = cloneFont(style);
+                // Toggle current bold state
+                font.setBold(!font.getBold());
+                style.setFont(font);
+                cell.setCellStyle(style);
+
+                cellsToRefresh.add(cell);
+            }
+            // Update all edited cells
+            spreadsheet.refreshCells(cellsToRefresh);
+        }
+    }
+
+    private void updateSelectedCellsBackgroundColor(Color newColor) {
+        if (spreadsheet != null && newColor != null) {
+            List<Cell> cellsToRefresh = new ArrayList<Cell>();
+            for (CellReference cellRef : spreadsheet
+                    .getSelectedCellReferences()) {
+                // Obtain Cell using CellReference
+                Cell cell = getOrCreateCell(cellRef);
+                // Clone Cell CellStyle
+                // This cast an only be done when using .xlsx files
+                XSSFCellStyle style = (XSSFCellStyle) cloneStyle(cell);
+                XSSFColor color = new XSSFColor(java.awt.Color.decode(newColor
+                        .getCSS()));
+                // Set new color value
+                style.setFillForegroundColor(color);
+                cell.setCellStyle(style);
+
+                cellsToRefresh.add(cell);
+            }
+            // Update all edited cells
+            spreadsheet.refreshCells(cellsToRefresh);
+        }
+    }
+
+    private void updateSelectedCellsFontColor(Color newColor) {
+        if (spreadsheet != null && newColor != null) {
+            List<Cell> cellsToRefresh = new ArrayList<Cell>();
+            for (CellReference cellRef : spreadsheet
+                    .getSelectedCellReferences()) {
+                Cell cell = getOrCreateCell(cellRef);
+                // Workbook workbook = spreadsheet.getWorkbook();
+                XSSFCellStyle style = (XSSFCellStyle) cloneStyle(cell);
+                XSSFColor color = new XSSFColor(java.awt.Color.decode(newColor
+                        .getCSS()));
+                XSSFFont font = (XSSFFont) cloneFont(style);
+                font.setColor(color);
+                style.setFont(font);
+                cell.setCellStyle(style);
+                cellsToRefresh.add(cell);
+            }
+            // Update all edited cells
+            spreadsheet.refreshCells(cellsToRefresh);
+        }
+    }
+
+    private Cell getOrCreateCell(CellReference cellRef) {
+        Cell cell = spreadsheet.getCell(cellRef.getRow(), cellRef.getCol());
+        if (cell == null) {
+            cell = spreadsheet.createCell(cellRef.getRow(), cellRef.getCol(),
+                    "");
+        }
+        return cell;
+    }
+
+    private CellStyle cloneStyle(Cell cell) {
+        CellStyle newStyle = spreadsheet.getWorkbook().createCellStyle();
+        newStyle.cloneStyleFrom(cell.getCellStyle());
+        return newStyle;
+    }
+
+    private Font cloneFont(CellStyle cellstyle) {
+        Font newFont = spreadsheet.getWorkbook().createFont();
+        Font originalFont = spreadsheet.getWorkbook().getFontAt(
+                cellstyle.getFontIndex());
+        if (originalFont != null) {
+            newFont.setBold(originalFont.getBold());
+            newFont.setItalic(originalFont.getItalic());
+            newFont.setFontHeight(originalFont.getFontHeight());
+            newFont.setUnderline(originalFont.getUnderline());
+            newFont.setStrikeout(originalFont.getStrikeout());
+            // This cast an only be done when using .xlsx files
+            XSSFFont originalXFont = (XSSFFont) originalFont;
+            XSSFFont newXFont = (XSSFFont) newFont;
+            newXFont.setColor(originalXFont.getXSSFColor());
+        }
+        return newFont;
+    }
+
+    @WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
+    @VaadinServletConfiguration(ui = MyUI.class, productionMode = false)
+    public static class MyUIServlet extends VaadinServlet {
+    }
+}
